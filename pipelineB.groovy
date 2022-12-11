639bc30fd7864174ef68cef656e32e6a31f60aa7
lurwas <lurwas@emacs.se>
2022-12-11 12:16:15 +0100

    fix syntax error in pipelineB.groovy
    * fix misc issues

--- pipelineB.groovy
+++ pipelineB.groovy
@@ -4,7 +4,7 @@ def PIPELINE_NUMBER = "${currentBuild.number}"
 def DoxygenTarFilename = "doc.tar.gz"
 def DoxygenConfigFilename = "doxygen_config.dox"
 
-node('java-agent') {
+node('master') {
     stage('load pipeline library') {
             pipelineLibrary = load 'pipeline.groovy'
     }
